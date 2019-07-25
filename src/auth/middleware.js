c7738d0de5c52d5f768f5455593194d9a79083c7
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-07-24 21:57:22 -0700

    Pushing files

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -6,13 +6,13 @@ module.exports = (req, res, next) => {
 
   try {
 
-    let [authType, encodedString] = req.headers.authorization.split(/\s+/);
+    let [authType, authString] = req.headers.authorization.split(/\s+/);
 
     // BASIC Auth  ... Authorization:Basic ZnJlZDpzYW1wbGU=
 
     switch(authType.toLowerCase()) {
       case 'basic':
-        return _authBasic(encodedString);
+        return _authBasic(authString);
       default:
         return _authError();
     }
@@ -21,17 +21,18 @@ module.exports = (req, res, next) => {
     return _authError();
   }
 
-  function _authBasic() {
+  function _authBasic(authString) {
     let base64Buffer = Buffer.from(authString,'base64'); // <Buffer 01 02...>
     let bufferString = base64Buffer.toString(); // john:mysecret
-    let [username,password] = bufferString.split(':');  // variables username="john" and password="mysecret"
-    let auth = [username,password];  // {username:"john", password:"mysecret"}
+    let [username, password] = bufferString.split(':');  // variables username="john" and password="mysecret"
+    let auth = {username, password};  // {username:"john", password:"mysecret"}
 
     return User.authenticateBasic(auth)
       .then( user => _authenticate(user) );
   }
 
   function _authenticate(user) {
+    console.log("This is the user", user)
     if ( user ) {
       next();
     }
